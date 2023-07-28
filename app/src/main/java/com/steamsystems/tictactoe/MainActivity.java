413e1872050ad5629560761d5917ccd68e119b8f
Abdulrehman Azam <abdulrehmana28@skiff.com>
2023-07-28 12:53:16 +0500

    game ready Better UI needed

--- app/src/main/java/com/steamsystems/tictactoe/MainActivity.java
+++ app/src/main/java/com/steamsystems/tictactoe/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -52,50 +53,86 @@ public class MainActivity extends AppCompatActivity {
 
     public void check(View v) {
         Button btnCurrent = (Button) v;
-        count++;
 
-        if(flag == 0){
-            btnCurrent.setText("X");
-            flag = 1;
-        } else{
-            btnCurrent.setText("O");
-            flag = 0;
+        if (btnCurrent.getText().toString().equals("")) {
+            count++;
+
+            if (flag == 0) {
+                btnCurrent.setText("X");
+                flag = 1;
+            } else {
+                btnCurrent.setText("O");
+                flag = 0;
+            }
+
+            if (count > 4) {
+                b1 = btn1.getText().toString();
+                b2 = btn2.getText().toString();
+                b3 = btn3.getText().toString();
+                b4 = btn4.getText().toString();
+                b5 = btn5.getText().toString();
+                b6 = btn6.getText().toString();
+                b7 = btn7.getText().toString();
+                b8 = btn8.getText().toString();
+                b9 = btn9.getText().toString();
+
+                // conditions horizontal
+                if (b1.equals(b2) && b2.equals(b3) && !b1.equals("")) {
+                    //1
+                    Toast.makeText(this, "Winner is " + b1, Toast.LENGTH_SHORT).show();
+                    newGame();
+                } else if (b4.equals(b5) && b5.equals(b6) && !b2.equals("")) {
+                    //2
+                    Toast.makeText(this, "Winner is " + b4, Toast.LENGTH_SHORT).show();
+                    newGame();
+                } else if (b7.equals(b8) && b8.equals(b9) && !b7.equals("")) {
+                    //3
+                    Toast.makeText(this, "Winner is " + b7, Toast.LENGTH_SHORT).show();
+                    newGame();
+                }
+                // vertical
+                else if (b1.equals(b4) && b4.equals(b7) && !b1.equals("")) {
+                    //4
+                    Toast.makeText(this, "Winner is " + b1, Toast.LENGTH_SHORT).show();
+                    newGame();
+                } else if (b2.equals(b5) && b5.equals(b8) && !b2.equals("")) {
+                    //5
+                    Toast.makeText(this, "Winner is " + b2, Toast.LENGTH_SHORT).show();
+                    newGame();
+                } else if (b3.equals(b6) && b6.equals(b9) && !b3.equals("")) {
+                    //6
+                    Toast.makeText(this, "Winner is " + b3, Toast.LENGTH_SHORT).show();
+                    newGame();
+                }
+                // cross condition
+                else if (b1.equals(b5) && b5.equals(b9) && !b5.equals("")) {
+                    //7
+                    Toast.makeText(this, "Winner is " + b1, Toast.LENGTH_SHORT).show();
+                    newGame();
+                } else if (b3.equals(b5) && b5.equals(b7) && !b5.equals("")) {
+                    //8
+                    Toast.makeText(this, "Winner is " + b3, Toast.LENGTH_SHORT).show();
+                    newGame();
+                } else if(count == 9){
+                    Toast.makeText(this, "Draw Game", Toast.LENGTH_SHORT).show();
+                    newGame();
+                }
+            }
         }
+    }
 
-        if(count > 4){
-            b1 = btn1.getText().toString();
-            b2 = btn2.getText().toString();
-            b3 = btn3.getText().toString();
-            b4 = btn4.getText().toString();
-            b5 = btn5.getText().toString();
-            b6 = btn6.getText().toString();
-            b7 = btn7.getText().toString();
-            b8 = btn8.getText().toString();
-            b9 = btn9.getText().toString();
-
-            // conditions horizontal
-            if(b1.equals(b2) && b2.equals(b3) && !b1.equals("")){
-                //1
-            } else if(b4.equals(b5) && b5.equals(b6) && !b2.equals("")){
-                //2
-            } else if(b7.equals(b8) && b8.equals(b9) && !b7.equals("")){
-                //3
-            }
-            // vertical
-            else if(b1.equals(b4) && b4.equals(b7) && !b1.equals("")){
-                //4
-            } else if(b2.equals(b5) && b5.equals(b8) && !b2.equals("")){
-                //5
-            } else if(b3.equals(b6) && b6.equals(b9) && !b3.equals("")){
-                //6
-            }
-            // cross condition
-            else if(b1.equals(b5) && b5.equals(b9) && !b5.equals("")){
-                //7
-            } else if(b3.equals(b5) && b5.equals(b7) && !b5.equals("")){
-                //8
-            }
-        }
+    public void newGame() {
+        btn1.setText("");
+        btn2.setText("");
+        btn3.setText("");
+        btn4.setText("");
+        btn5.setText("");
+        btn6.setText("");
+        btn7.setText("");
+        btn8.setText("");
+        btn9.setText("");
+        count = 0;
+        flag = 0;
     }
 
 }
